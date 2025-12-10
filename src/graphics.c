ec3be480b82c7c7f5982692ef737b3357f392c08
sujan0629 <sujanbhatta0629@gmail.com>
2025-12-10 12:30:05 +0545

    graphics: add SDL placeholder for visualization

---
+++ src/graphics.c
@@ -0,0 +1,12 @@
+// Placeholder for SDL graphics
+// TODO: Install SDL2, include <SDL2/SDL.h>
+// Initialize window, render lanes, lights, vehicles
+
+#include <stdio.h>
+
+int main() {
+    printf("Graphics module - SDL integration pending\n");
+    // SDL_Init(SDL_INIT_VIDEO);
+    // ... render loop
+    return 0;
+}
