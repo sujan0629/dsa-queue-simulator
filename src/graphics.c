61bbeb185df4845082eca5d40d2c835136c17108
sujan0629 <sujanbhatta0629@gmail.com>
2025-12-11 09:15:30 +0545

    graphics: implement basic SDL window and lane rendering

--- src/graphics.c
+++ src/graphics.c
@@ -1,12 +1,75 @@
-// Placeholder for SDL graphics
-// TODO: Install SDL2, include <SDL2/SDL.h>
-// Initialize window, render lanes, lights, vehicles
-
+#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define LANE_WIDTH 100
+#define LANE_HEIGHT 400
+
+void draw_lane(SDL_Renderer* renderer, int x, int y, int width, int height) {
+    SDL_Rect rect = {x, y, width, height};
+    SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255); // Gray
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+void draw_light(SDL_Renderer* renderer, int x, int y, SDL_Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_Rect rect = {x, y, 20, 20};
+    SDL_RenderFillRect(renderer, &rect);
+}
 
 int main() {
-    printf("Graphics module - SDL integration pending\n");
-    // SDL_Init(SDL_INIT_VIDEO);
-    // ... render loop
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("DSA Traffic Simulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer creation failed: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    int running = 1;
+    SDL_Event event;
+    SDL_Color red = {255, 0, 0, 255};
+    SDL_Color green = {0, 255, 0, 255};
+    int light_state = 0; // 0 red, 1 green
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = 0;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw lanes
+        for (int i = 0; i < 4; i++) {
+            draw_lane(renderer, 50 + i * 150, 100, LANE_WIDTH, LANE_HEIGHT);
+        }
+
+        // Draw lights
+        SDL_Color light_color = light_state ? green : red;
+        draw_light(renderer, 350, 50, light_color);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000); // 1 second
+        light_state = !light_state;
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
