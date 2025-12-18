0ce2cfddda2c369ba30ba756e8eeabacea90100b
sujan0629 <sujanbhatta0629@gmail.com>
2025-12-18 16:45:35 +0545

    demo: update script for socket-based communication

--- demo.sh
+++ demo.sh
@@ -6,10 +6,18 @@ echo "Starting DSA Queue Simulator Demo"
 # Build all
 make
 
-# Run in background
-./traffic_generator &
-./reciever &
-./simulator
+# Run simulator in background
+./simulator &
+SIM_PID=$!
+
+# Wait a bit
+sleep 2
+
+# Run generator
+./traffic_generator
+
+# Kill simulator
+kill $SIM_PID
 
 # Cleanup
 make clean
