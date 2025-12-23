b6f90df41615ec1e85b0944c33f2fac6dc0f8b85
sujan0629 <sujanbhatta0629@gmail.com>
2025-12-23 11:30:00 +0545

    fix: add windows.h for Sleep function on Windows

--- src/simulator.c
+++ src/simulator.c
@@ -14,6 +14,7 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
 #define sleep(x) Sleep(x * 1000)
 #endif
--- src/traffic_generator.c
+++ src/traffic_generator.c
@@ -11,7 +11,9 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
+#define sleep(x) Sleep(x * 1000)
 #endif
 
 #define NUM_LANES 4
